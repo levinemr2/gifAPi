21cd016f142ed02d3c7f179a3502ec41667a2021
levinemr2 <levinemr2@vcu.edu>
2019-03-23 09:54:33 -0400

    added details

---
+++ app.js
@@ -0,0 +1,51 @@
+
+
+
+var shows = ["parks and rec", "friends", "scrubs"];
+
+          function displayGif() {
+              var show = $(this).attr("data-name");
+              var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + shows + "&api_key=dc6zaTOxFJmzC&limit=10";
+              console.log(queryURL);
+
+              $.ajax({
+                  url: queryURL,
+                  method: "GET",
+              }).then(function(response) {
+                  $("#tvDump").prepend(`<div class='showGif'>
+                    <img src='${response.data.image_original_url}')>
+                    </div>
+                    `);
+              });
+          }
+
+         function renderButtons() {
+             $("#buttons").empty();
+
+             for (var i = 0; i < shows.length; i++) {
+                 var a = $("<button>");
+                    a.addClass("tv-btn");
+                    a.attr("data-name", shows[i]);
+                    a.text(shows[i]);
+                    $("#buttons").append(a);
+             }
+         }
+
+         $("#add-show").on("click", function(event){
+             event.preventDefault();
+             var show = $("#show-input").val().trim();
+
+             shows.push(show);
+
+             renderButtons();
+         });
+
+         $(document).on("click", ".tv-btn", displayGif);
+          
+         renderButtons();
+
+     
+
+        
+          
+     
